5f10c00f12c34d6a09e65fff74eabcbc2efae7a6
zhuli51 <zhuli51@jd.com>
2023-11-27 13:47:57 +0800

    feat: 提交

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const eg = 'hahah111'
 console.log('eg', Object.prototype.toString.call(eg).split(' ')[1].split(']')[0], '========',eg);
-
-
+console.log('eg', Object.prototype.toString.call(eg).split(' ')[1].split(']')[0], '========',eg);
+console.log('======',)
 
